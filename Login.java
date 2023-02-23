a0762f380df2310542f3fdebde72e5502f0a482a
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2023-02-23 17:50:18 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -4,10 +4,59 @@
  */
 package healthappsc;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
- * @author Ethan
+ * @author Ethan Judice
  */
 public class Login {
+    
+    public static String LoginPrompt() throws FileNotFoundException{
+        
+        String UserData = "";
+        String TextDoc = "UserInfo.txt";
+        Scanner UserImput = new Scanner(System.in);
+        while(UserData == ""){
+            Scanner inFile = new Scanner(new FileReader(TextDoc));
+            System.out.println("Please enter Username and Password, seperated "
+                             + "by a space: ");
+            String UserImputString = UserImput.nextLine();
+            String[] splitted = UserImputString.split("\\s+");
+            if(splitted.length == 2){
+                int line = 0;
+                while(inFile.hasNextLine()){
+                    line++;
+                    if(inFile.nextLine().contains(UserImputString)){
+                        UserData = getUserData(line);
+                        return UserData;
+                    }
+                }
+                System.out.println("Username or Password not recognized.");
+            }
+            else{
+                System.out.println("Format not recognized, try again.");
+            }
+            
+        }
+        
+        return "ERROR";
+        
+    }
+    
+    public static String getUserData(int line) throws FileNotFoundException{
+        
+        String TextDoc = "UserInfo.txt";
+        String Data = "";
+        Scanner inFile = new Scanner(new FileReader(TextDoc));
+        for(int i=0;i<line;i++){
+            Data = inFile.nextLine();
+        }
+        
+        return Data;
+    }
+    
     
 }
