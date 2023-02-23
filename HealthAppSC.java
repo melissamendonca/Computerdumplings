c1d79f7f4f0455b38394528ddfeb9ad4eb89ed10
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2023-02-23 17:49:58 -0600

    Update HealthAppSC.java

--- HealthAppSC.java
+++ HealthAppSC.java
@@ -18,9 +18,9 @@ public class HealthAppSC {
      */
     public static void main(String[] args) throws FileNotFoundException{
         
-        String UserDataString = Login();
+        String UserDataString = Login.LoginPrompt();
         String OldData = UserDataString;
-        System.out.println("UserData is: " + UserDataString);
+        //in text document, NewData will replace OldData
         String[] UserDataSplited = UserDataString.split("\\s+");
         /*
         UserDataSplited[0] is Username
@@ -32,53 +32,11 @@ public class HealthAppSC {
         UserDataSplited[6] is WaterTracker (daily intake in ounces)
         UserDataSplited[7] is WorkoutKeys (keys seperated by only commas)
         */
+        User UserData = new User();
+        UserData.UserSetData(UserDataSplited);
         
         
     }
     
-    public static String Login() throws FileNotFoundException{
-        
-        String UserData = "";
-        String TextDoc = "UserInfo.txt";
-        Scanner UserImput = new Scanner(System.in);
-        while(UserData == ""){
-            Scanner inFile = new Scanner(new FileReader(TextDoc));
-            System.out.println("Please enter Username and Password, seperated "
-                             + "by a space: ");
-            String UserImputString = UserImput.nextLine();
-            String[] splitted = UserImputString.split("\\s+");
-            if(splitted.length == 2){
-                int line = 0;
-                while(inFile.hasNextLine()){
-                    line++;
-                    if(inFile.nextLine().contains(UserImputString)){
-                        UserData = getData(line);
-                        return UserData;
-                    }
-                }
-                System.out.println("Username or Password not recognized.");
-            }
-            else{
-                System.out.println("Format not recognized, try again.");
-            }
-            
-        }
-        
-        return null;
-        
-    }
-    
-    public static String getData(int line) throws FileNotFoundException{
-        
-        String TextDoc = "UserInfo.txt";
-        String Data = "";
-        System.out.println(line);
-        Scanner inFile = new Scanner(new FileReader(TextDoc));
-        for(int i=0;i<line;i++){
-            Data = inFile.nextLine();
-        }
-        
-        return Data;
-    }
     
 }
