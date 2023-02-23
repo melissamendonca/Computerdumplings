1ff4ba10a804bdc37bc2cb689bd3718e7c990140
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2023-02-23 17:50:56 -0600

    Update User.java

--- User.java
+++ User.java
@@ -6,8 +6,61 @@ package healthappsc;
 
 /**
  *
- * @author Ethan
+ * @author Ethan Judice
  */
 public class User {
+    
+    String Username = null;
+    String Password = null;
+    int Height = 0;
+    int Weight = 0;
+    int BMI = 0;
+    int CalorieTracker = 0;
+    int WaterTracker = 0;
+    String WorkoutKeys = null;
+    
+    
+    public void UserSetData(String[] Data){
+        Username = Data[0];
+        Password = Data[1];
+        Height = Integer.parseInt(Data[2]);
+        Weight = Integer.parseInt(Data[3]);
+        BMI = Integer.parseInt(Data[4]);
+        CalorieTracker = Integer.parseInt(Data[5]);
+        WaterTracker = Integer.parseInt(Data[6]);
+        WorkoutKeys = Data[7];
+    }
+    
+    public String getUsername(){
+        return Username;
+    }
+    
+    public String getPassword(){
+        return Password;
+    }
+    
+    public int getHeight(){
+        return Height;
+    }
+    
+    public int getWeight(){
+        return Weight;
+    }
+    
+    public int getBMI(){
+        return BMI;
+    }
+    
+    public int getCalories(){
+        return CalorieTracker;
+    }
+    
+    public int getWater(){
+        return WaterTracker;
+    }
+    
+    public String getWorkoutKeys(){
+        return WorkoutKeys;
+    }
     
 }
