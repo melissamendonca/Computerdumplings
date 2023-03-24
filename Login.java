591955e5717df5608759eb7596959dd4cecab954
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2023-03-24 18:16:24 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -16,34 +16,56 @@ public class Login {
     
     public static String LoginPrompt() throws FileNotFoundException{
         
+        
         String UserData = "";
         String TextDoc = "UserInfo.txt";
         Scanner UserImput = new Scanner(System.in);
-        while(UserData == ""){
-            Scanner inFile = new Scanner(new FileReader(TextDoc));
-            System.out.println("Please enter Username and Password, seperated "
-                             + "by a space: ");
+        Boolean isNewUser = null;
+        while(isNewUser == null){
+            System.out.println("Are you a new user? Type Yes or No");
             String UserImputString = UserImput.nextLine();
-            String[] splitted = UserImputString.split("\\s+");
-            if(splitted.length == 2){
-                int line = 0;
-                while(inFile.hasNextLine()){
-                    line++;
-                    if(inFile.nextLine().contains(UserImputString)){
-                        UserData = getUserData(line);
-                        return UserData;
-                    }
-                }
-                System.out.println("Username or Password not recognized.");
+            if(UserImputString == "Yes"){
+                isNewUser = true;
+            }
+            if(UserImputString == "No"){
+                isNewUser = false;
             }
             else{
-                System.out.println("Format not recognized, try again.");
+                System.out.println("Command not recognized, please try again.");
             }
-            
         }
         
-        return "ERROR";
         
+        if(isNewUser == true){
+            while(UserData == ""){
+                Scanner inFile = new Scanner(new FileReader(TextDoc));
+                System.out.println("Please enter Username and Password, "
+                                 + "seperated by a space: ");
+                String UserImputString = UserImput.nextLine();
+                String[] splitted = UserImputString.split("\\s+");
+                if(splitted.length == 2){
+                    int line = 0;
+                    while(inFile.hasNextLine()){
+                        line++;
+                        if(inFile.nextLine().contains(UserImputString)){
+                            UserData = getUserData(line);
+                            return UserData;
+                        }
+                    }
+                    System.out.println("Username or Password not recognized.");
+                }
+                else{
+                    System.out.println("Format not recognized, try again.");
+                }
+            
+            }
+        return "ERROR";
+    }
+    if(isNewUser == false){
+        newUser();
+    }    
+        
+        return "ERROR";
     }
     
     public static String getUserData(int line) throws FileNotFoundException{
@@ -56,6 +78,13 @@ public class Login {
         }
         
         return Data;
+    }
+    
+    public static String newUser() throws FileNotFoundException{
+        
+        //must have database format completed//
+        String data = "";
+        return data;
     }
     
     
